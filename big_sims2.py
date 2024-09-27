5378e92e20284ca7266dada6b8f57bae67ed9614
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-09-27 18:29:07 +0100

    sbm fix params in sims

--- big_sims2.py
+++ big_sims2.py
@@ -15,7 +15,10 @@ taus = np.arange(0.01,0.31, 0.01)
 
 for model in models: 
     contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
-    params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
+    if model == models[0]:
+        params = []
+    else:
+        params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
     results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
     with open(f'../output_data/simulations/big/{data}_{model}.json', 'w') as file:
         json.dump(results, file)
