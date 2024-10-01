465c3ce8cffffeee32eb94911b2c6504a89832c4
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-10-01 20:09:31 +0100

    network characterising

--- _characterize_networks.py
+++ _characterize_networks.py
@@ -4,10 +4,10 @@ import numpy as np
 import networkx as nx
 import json
 
-n, iters = 100_000, 1_000
+ns, iters = np.arange(10_000,210_000,10_000), 50
 
 buckets = np.array([5,12,18,30,40,50,60,70])
-partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
+
 
 datas = ['poly','comix1', 'comix2']
 models = ['sbm', 'nbinom', 'dpln']
@@ -15,26 +15,41 @@ scale1 = 'fit2'
 
 for data in datas:
     for model in models:
+        degrees, assort, clustering, diameters, short_paths = np.zeros((len(ns), iters)), np.zeros((len(ns), iters)), np.zeros((len(ns), iters)), np.zeros((len(ns), iters)), np.zeros((len(ns), iters))
         print(f'{data}: {model}')
         contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
         if model == 'sbm':
             params=[]
         else:
             params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
-        network = nd_p.build_network(n,partitions,contact_matrix,params,model)
-        G = nd_p.to_networkx(network=network)
-        # Compute the average degree
-        degrees = dict(G.degree())
-        average_degree = sum(degrees.values()) / float(G.number_of_nodes())
+        for i, n in enumerate(ns):
+            print(n)
+            partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
+            for j in range(iters):
+                
+                network = nd_p.build_network(n,partitions,contact_matrix,params,model)
+                G = nd_p.to_networkx(network=network)
+                # Compute the average degree
+                degrees = dict(G.degree())
+                average_degree = sum(degrees.values()) / float(G.number_of_nodes())
 
-        # Compute the degree assortativity coefficient
-        degree_assortativity = nx.degree_assortativity_coefficient(G)
+                # Compute the degree assortativity coefficient
+                degree_assortativity = nx.degree_assortativity_coefficient(G)
 
-        # Compute the average clustering coefficient
-        average_clustering = nx.average_clustering(G)
+                # Compute the average clustering coefficient
+                average_clustering = nx.average_clustering(G)
+                
+                if nx.is_connected(G):
+                    diameter = nx.diameter(G)
+                    short_path = nx.average_shortest_path_length(G)
+                else:
+                    diameter = -1
+                    short_path = -1
 
-        # Print the results
-        print("what it says:", np.mean(network['degrees']))
-        print("Average Degree:", average_degree)
-        print("Degree Assortativity Coefficient:", degree_assortativity)
-        print("Average Clustering Coefficient:", average_clustering)
+                degrees[i,j] = average_degree; assort[i,j] = degree_assortativity; clustering[i,j] = average_clustering; diameters[i,j] = diameter; short_paths[i,j] = short_path 
+        np.savetxt(f'../output_data/characterising/degrees_{data}_{model}.csv', degrees)
+        np.savetxt(f'../output_data/characterising/assort_{data}_{model}.csv', assort)
+        np.savetxt(f'../output_data/characterising/clust_{data}_{model}.csv', clustering)
+        np.savetxt(f'../output_data/characterising/degrees_{data}_{model}.csv', diameters)
+        np.savetxt(f'../output_data/characterising/short_path_{data}_{model}.csv', short_paths)
+        
