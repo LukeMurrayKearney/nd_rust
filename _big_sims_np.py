814a88acb5cda89067fe7529b18aeb1d49f25265
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-10-01 17:56:53 +0100

    fix no age sims

--- _big_sims_np.py
+++ _big_sims_np.py
@@ -13,22 +13,22 @@ models = ['sbm', 'dpln']
 scale = 'none'
 taus = np.array([0.02,0.03,0.04,0.05,0.06])
 
-for model in models: 
-    contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
-    if model == models[0]:
-        params = []
-    else:
-        params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
-    results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
-    with open(f'../output_data/simulations/big/right_{data}_{model}.json', 'w') as file:
-        json.dump(results, file)
+# for model in models: 
+#     contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
+#     if model == models[0]:
+#         params = []
+#     else:
+#         params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
+#     results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
+#     with open(f'../output_data/simulations/big/right_{data}_{model}.json', 'w') as file:
+#         json.dump(results, file)
     
-# no age
+# # no age
 
 buckets = np.array([])
 partitions = [n]
 
 egos, contact_matrix, params = nd_p.fit_to_data(input_file_path=f'input_data/{data}.csv',dist_type=models[1], buckets=buckets)
 results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=models[1], prop_infec=10/n, scaling=scale)
-with open(f'../output_data/simulations/big/noAge_{data}_{model[1]}.json', 'w') as file:
+with open(f'../output_data/simulations/big/noAge_{data}_{models[1]}.json', 'w') as file:
     json.dump(results, file)
