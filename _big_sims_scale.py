a58896a695f60a569b5745877b8505904cf3933d
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-10-01 18:26:58 +0100

    big sims scaled 2

--- _big_sims_scale.py
+++ _big_sims_scale.py
@@ -10,7 +10,10 @@ partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.8
 
 datas = ['comix2','comix1']
 model = 'dpln'
-taus = np.array([0.2,0.21,0.22,0.23,0.24,0.25,0.3,0.4,0.42,0.43,0.44,0.45,0.46,0.47,0.48,0.49,0.5])
+## attempt 1
+# taus = np.array([0.2,0.21,0.22,0.23,0.24,0.25,0.3,0.4,0.42,0.43,0.44,0.45,0.46,0.47,0.48,0.49,0.5])
+## attempt 2
+taus = np.array([0.1 + 0.01*a for a in range(10)])
 
 print(1)
 data = datas[0]
@@ -18,7 +21,7 @@ scale = 'fit2'
 contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
 params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
 results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
-with open(f'../output_data/simulations/big/right_{data}_{model}_{scale}.json', 'w') as file:
+with open(f'../output_data/simulations/big/right_{data}_{model}_{scale}2.json', 'w') as file:
     json.dump(results, file)
     
 print(2)
@@ -27,5 +30,5 @@ scale = 'fit1'
 contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
 params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
 results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
-with open(f'../output_data/simulations/big/right_{data}_{model}_{scale}.json', 'w') as file:
+with open(f'../output_data/simulations/big/right_{data}_{model}_{scale}2.json', 'w') as file:
     json.dump(results, file)
