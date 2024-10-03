876c812a1700cefa24614585e381540f5fa26274
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-10-03 13:03:50 +0100

    no age sims

--- _big_sims_nc1.py
+++ _big_sims_nc1.py
@@ -3,7 +3,7 @@ import nd_python as nd_p
 import numpy as np
 import json
 
-n, iters = 100_000, 100
+n, iters = 100_000, 30
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
@@ -18,22 +18,22 @@ scale = 'none'
 ## attempt 3
 taus = np.array([0.0025,0.005,0.0075,0.01,0.02,0.03,0.04,0.06,0.08,0.01,0.12,0.14,0.015,0.025,0.035,0.045,0.05,0.055,0.07,0.09,0.11,0.13])
 
-for model in models: 
-    contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
-    if model == models[0]:
-        params = []
-    else:
-        params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
-    results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
-    with open(f'../output_data/simulations/big/right_{data}_{model}3.json', 'w') as file:
-        json.dump(results, file)
+# for model in models: 
+#     contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
+#     if model == models[0]:
+#         params = []
+#     else:
+#         params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
+#     results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
+#     with open(f'../output_data/simulations/big/right_{data}_{model}3.json', 'w') as file:
+#         json.dump(results, file)
     
 # no age
 
-# buckets = np.array([])
-# partitions = [n]
+buckets = np.array([])
+partitions = [n]
 
-# egos, contact_matrix, params = nd_p.fit_to_data(input_file_path=f'input_data/{data}.csv',dist_type=models[1], buckets=buckets, save_fig=False)
-# results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=models[1], prop_infec=10/n, scaling=scale)
-# with open(f'../output_data/simulations/big/noAge_{data}_{models[1]}3.json', 'w') as file:
-#     json.dump(results, file)
+egos, contact_matrix, params = nd_p.fit_to_data(input_file_path=f'input_data/{data}.csv',dist_type=models[1], buckets=buckets, save_fig=False)
+results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=models[1], prop_infec=10/n, scaling=scale)
+with open(f'../output_data/simulations/big/noAge_{data}_{models[1]}.json', 'w') as file:
+    json.dump(results, file)
