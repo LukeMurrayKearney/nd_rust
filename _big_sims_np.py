4b853f9ca5f1c11e129c2c8fe5f275d4706a593c
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-10-11 17:54:34 +0100

    finishing big sims

--- _big_sims_np.py
+++ _big_sims_np.py
@@ -3,7 +3,7 @@ import nd_python as nd_p
 import numpy as np
 import json
 
-n, iters = 100_000, 30
+n, iters = 100_000, 100
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
@@ -16,17 +16,18 @@ scale = 'none'
 ## attempt 2
 # taus = np.array([0.005,0.01,0.015,0.025,0.035,0.045,0.055,0.065,0.07,0.075,0.08,0.02,0.03,0.04,0.05,0.06])
 ## attempt 3
-taus = np.array([0.005,0.01,0.015,0.025,0.035,0.045,0.055,0.065,0.07,0.075,0.08,0.02,0.03,0.04,0.05,0.06])
+# taus = np.array([0.005,0.01,0.015,0.025,0.035,0.045,0.055,0.065,0.07,0.075,0.08,0.02,0.03,0.04,0.05,0.06])
+## attempt 4
+taus = [np.array([0.0025,0.0075,0.085,0.09,0.095,0.1]), np.arange(0.001,0.01,0.001)]
 
-
-for model in models: 
+for i, model in enumerate(models): 
     contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
     if model == models[0]:
         params = []
     else:
         params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
-    results = nd_p.taus_sims(taus=taus, partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
-    with open(f'../output_data/simulations/big/right_{data}_{model}3.json', 'w') as file:
+    results = nd_p.taus_sims(taus=taus[i], partitions=partitions, contact_matrix=contact_matrix,network_params=params, iterations=iters, n=n, dist_type=model, prop_infec=10/n, scaling=scale)
+    with open(f'../output_data/simulations/big/right_{data}_{model}4.json', 'w') as file:
         json.dump(results, file)
     
 # # no age
