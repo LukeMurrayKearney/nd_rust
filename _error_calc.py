b57d0388724116edfb99f9f84a7890a82c7a6dfb
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-10-11 15:22:24 +0100

    errors  with themselves

--- _error_calc.py
+++ _error_calc.py
@@ -30,34 +30,34 @@ for data in datas:
 
             # my model error
             network = nd_p.build_network(n,partitions,contact_matrix,params,model)
-            errors, err_pp = nd_p.emd_error(egos, network, distance_matrix=distance_matrix)
-            error_breakdown.append(errors)
-            error.append(err_pp)
+            # errors, err_pp = nd_p.emd_error(egos, network, distance_matrix=distance_matrix)
+            # error_breakdown.append(errors)
+            # error.append(err_pp)
             
             
-            # # network error of my model with true network
-            # data = nd_p.data_from_network(network=network)
-            # egos_itself, contact_matrix_itself, params_itself = nd_p.fit_to_data(df=data, save_fig=False, output_file_path="fits/network_comix1", buckets=buckets,dist_type=model)
-            # network = nd_p.build_network(n, partitions, params_itself, contact_matrix_itself,dist_type=model)
-            # errors, err_pp = nd_p.emd_error(egos_itself, network, distance_matrix=distance_matrix)
-            # error_with_itself_breakdown.append(errors)
-            # error_with_itself.append(err_pp)
+            # network error of my model with true network
+            data = nd_p.data_from_network(network=network)
+            egos_itself, contact_matrix_itself, params_itself = nd_p.fit_to_data(df=data, save_fig=False, output_file_path="fits/network_comix1", buckets=buckets,dist_type=model)
+            network = nd_p.build_network(n, partitions, params_itself, contact_matrix_itself,dist_type=model)
+            errors, err_pp = nd_p.emd_error(egos_itself, network, distance_matrix=distance_matrix)
+            error_with_itself_breakdown.append(errors)
+            error_with_itself.append(err_pp)
             
-            with open(f'../output_data/errors/breakdown_{data}_{model}.csv', 'w', newline='') as file:
-                writer = csv.writer(file)
-                for row in error_breakdown:
-                    writer.writerow(row)
-                    
-            # with open(f'../output_data/errors/breakdown_itself_{data}_{model}.csv', 'w', newline='') as file:
+            # with open(f'../output_data/errors/breakdown_{data}_{model}.csv', 'w', newline='') as file:
             #     writer = csv.writer(file)
-            #     for row in error_with_itself_breakdown:
+            #     for row in error_breakdown:
             #         writer.writerow(row)
-
-            with open(f'../output_data/errors/{data}_{model}.csv', 'w', newline='') as file:
+                    
+            with open(f'../output_data/errors/breakdown_itself_{data}_{model}.csv', 'w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(error)
+                for row in error_with_itself_breakdown:
+                    writer.writerow(row)
 
-
-            # with open(f'../output_data/errors/itself_{data}_{model}.csv', 'w', newline='') as file:
+            # with open(f'../output_data/errors/{data}_{model}.csv', 'w', newline='') as file:
             #     writer = csv.writer(file)
-            #     writer.writerow(error_with_itself)
+            #     writer.writerow(error)
+
+
+            with open(f'../output_data/errors/itself_{data}_{model}.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(error_with_itself)
