e541c6c8a75acc21b18548ad5b47f2330884e876
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-12-10 13:19:39 +0000

    new no age sims 0

--- _final_sellke_noage_c2.py
+++ _final_sellke_noage_c2.py
@@ -22,9 +22,9 @@ scales = ['fit2']
 
 ## 0,1
 # taus = [[np.arange(0.001,0.07,0.002)]]
-# taus = [[np.arange(0.001,0.07,0.002)]]
+taus = [[np.arange(0.001,0.07,0.002)]]
 # taus = [[np.arange(0.001,0.05,0.002)]]
-# taus = [[10*x for x in a] for a in taus]
+taus = [[10*x for x in a] for a in taus]
 ## 2,3
 # taus = [[np.arange(0.001,0.07,0.001)]]
 # taus = [[np.arange(0.001,0.07,0.002)]]
@@ -37,14 +37,14 @@ scales = ['fit2']
 # taus = [[15*x for x in a] for a in taus]
 ## 6,7
 # taus = [[np.arange(0.001,0.07,0.001)]]
-taus = [[np.arange(0.001,0.07,0.002)]]
+# taus = [[np.arange(0.001,0.07,0.002)]]
 # taus = [[np.arange(0.001,0.05,0.002)]]
-taus = [[30*x for x in a] for a in taus]
+# taus = [[30*x for x in a] for a in taus]
 ## 8,9
 # taus = [[np.arange(0.001,0.07,0.001)]]
-taus = [[np.arange(0.001,0.07,0.002)]]
+# taus = [[np.arange(0.001,0.07,0.002)]]
 # taus = [[np.arange(0.001,0.05,0.002)]]
-taus = [[10*x for x in a] for a in taus]
+# taus = [[10*x for x in a] for a in taus]
 
 
 for i, data in enumerate(datas):
@@ -57,7 +57,7 @@ for i, data in enumerate(datas):
         #     params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
         df = pd.read_csv(f'input_data/{data}.csv')
         _, contact_matrix, params = nd_p.fit_to_data(df=df,dist_type=model, buckets=buckets, save_fig=False)
-        result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=5/n, scaling=scales[j])
-        with open(f'../output_data/simulations/big/sellke/SIR/8_{data}_{model}_{scales[j]}_noage.json','w') as f:
+        result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=10/n, scaling=scales[j])
+        with open(f'../output_data/simulations/big/sellke/SIR/0_{data}_{model}_{scales[j]}_noage.json','w') as f:
             json.dump(result, f)
 print('done')
--- _final_sellke_noage_p.py
+++ _final_sellke_noage_p.py
@@ -23,8 +23,8 @@ scales = ['fit1']
 ## 0,1
 # taus = [[np.arange(0.001,0.07,0.002)]]
 # # taus = [[np.arange(0.001,0.07,0.002)]]
-# taus = [[np.arange(0.001,0.05,0.002)]]
-# taus = [[10*x for x in a] for a in taus]
+taus = [[np.arange(0.001,0.05,0.002)]]
+taus = [[10*x for x in a] for a in taus]
 ## 2,3
 # taus = [[np.arange(0.001,0.07,0.001)]]
 # taus = [[np.arange(0.001,0.07,0.002)]]
@@ -43,8 +43,8 @@ scales = ['fit1']
 ## 8,9
 # taus = [[np.arange(0.001,0.07,0.001)]]
 # taus = [[np.arange(0.001,0.07,0.002)]]
-taus = [[np.arange(0.001,0.05,0.002)]]
-taus = [[10*x for x in a] for a in taus]
+# taus = [[np.arange(0.001,0.05,0.002)]]
+# taus = [[10*x for x in a] for a in taus]
 
 for i, data in enumerate(datas):
     for j, model in enumerate(models):
@@ -56,7 +56,7 @@ for i, data in enumerate(datas):
         #     params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
         df = pd.read_csv(f'input_data/{data}.csv')
         _, contact_matrix, params = nd_p.fit_to_data(df=df,dist_type=model, buckets=buckets, save_fig=False)
-        result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=5/n, scaling=scales[j])
-        with open(f'../output_data/simulations/big/sellke/SIR/8_{data}_{model}_{scales[j]}_noage.json','w') as f:
+        result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=10/n, scaling=scales[j])
+        with open(f'../output_data/simulations/big/sellke/SIR/0_{data}_{model}_{scales[j]}_noage.json','w') as f:
             json.dump(result, f)
 print('done')
