6e7bf17ab91642ec937dc7828e44ec3695eafc4b
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-12-12 17:37:41 +0000

    fix run missing

--- _run_missing.py
+++ _run_missing.py
@@ -6,10 +6,8 @@ n, iters = 100_000, 48
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
 
-# datas = ['comix1','comix2','poly']
-datas = ['poly']
-# models = ['sbm','dpln']
-models = ['dpln']
+datas = ['comix1','comix2','poly']
+models = ['sbm','dpln']
 scales = ['none']
 
 for sim_num in [0,1,2,10,11,12,13,14,15,16,17,18,19]:
@@ -45,14 +43,16 @@ for sim_num in [0,1,2,10,11,12,13,14,15,16,17,18,19]:
                 [np.arange(0.001,0.05,0.001), np.arange(0.0001,0.03,0.0001)]]
         
     for i, data in enumerate(datas):
-        for j, model in enumerate(models):
-            print(data, model)
-            contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
-            if model == 'sbm':
-                params = []
-            else:
-                params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
-            result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=10/n, scaling=scales[j])
-            with open(f'../output_data/simulations/big/sellke/SIR/{sim_num}_{data}_{model}_{scales[j]}.json','w') as f:
-                json.dump(result, f)
+        if data == 'poly':
+            for j, model in enumerate(models):
+                if model == 'dpln':
+                    print(data, model)
+                    contact_matrix = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
+                    if model == 'sbm':
+                        params = []
+                    else:
+                        params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
+                    result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=10/n, scaling=scales[j])
+                    with open(f'../output_data/simulations/big/sellke/SIR/{sim_num}_{data}_{model}_{scales[j]}.json','w') as f:
+                        json.dump(result, f)
 print('done')
