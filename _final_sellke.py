74c86868ff85563b0da350c5a8bf7b78bac9f6c1
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2024-12-16 17:56:47 +0000

    sims all

--- _final_sellke.py
+++ _final_sellke.py
@@ -34,10 +34,14 @@ scales = ['none', 'none']
 # taus = [[np.arange(0.005,0.1,0.005), np.arange(0.0005,0.005,0.0005)],
 #         [np.arange(0.005,0.1,0.005), np.arange(0.00005,0.001,0.00005)],
 #         [np.arange(0.001,0.05,0.001), np.arange(0.0001,0.03,0.0001)]]
-# 20,21
-taus = [[np.arange(0.005,0.05,0.005), np.arange(0.0001,0.005,0.0001)],
-        [np.arange(0.005,0.05,0.005), np.arange(0.00005,0.001,0.00005)],
-        [np.arange(0.001,0.025,0.001), np.arange(0.0005,0.015,0.0005)]]
+# 20,21,22
+# taus = [[np.arange(0.005,0.05,0.005), np.arange(0.0001,0.005,0.0001)],
+#         [np.arange(0.005,0.05,0.005), np.arange(0.00005,0.001,0.00005)],
+#         [np.arange(0.001,0.025,0.001), np.arange(0.0005,0.015,0.0005)]]
+# 23,24
+taus = [[np.arange(0.1,0.205,0.005), np.arange(0.0005,0.01,0.0005)],
+        [np.arange(0.05,0.2,0.01), np.arange(0.0005,0.01,0.0005)],
+        [np.arange(0.001,0.1,0.005), np.arange(0.0025,0.05,0.0025)]]
 
 ### long
 ## 0,1,2
@@ -54,6 +58,6 @@ for i, data in enumerate(datas):
         else:
             params = np.genfromtxt(f'input_data/parameters/params_{data}_{model}.csv', delimiter=',')
         result = nd_p.big_sellke_sims(partitions=partitions,contact_matrix=contact_matrix,network_params=params,n=n,dist_type=model,num_networks=1,iterations=iters, taus=taus[i][j],prop_infec=10/n, scaling=scales[j])
-        with open(f'../output_data/simulations/big/sellke/SIR/22_{data}_{model}_{scales[j]}.json','w') as f:
+        with open(f'../output_data/simulations/big/sellke/SIR/23_{data}_{model}_{scales[j]}.json','w') as f:
             json.dump(result, f)
 print('done')
